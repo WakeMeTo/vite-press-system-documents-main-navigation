f0de3563fa7eae3bf363b6ba376d6e735e17cab3
wake <1487419708@qq.com>
2023-07-27 12:59:00 +0800

    Added Algolia Search function

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -24,9 +24,12 @@ export default defineConfig({
         // link => <link>标签
         //['link', {rel: 'icon', type: 'image/svg+xml', href: '/StreamlineEmojis2.svg'}],
 
-        ['meta', {name: 'viewport', content: 'width=device-width,initial-scale=1,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no'}],
+        ['meta', {
+            name: 'viewport',
+            content: 'width=device-width,initial-scale=1,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no'
+        }],
 
-        // 网站图标 => <link rel="icon" href="/favicon.ico">
+        // 网站图标 => <link rel="icon" href="favicon.ico">,favicon.ico前面不能加/，否则找不到
         ['link', {rel: 'icon', href: 'favicon.ico'}]
     ],
 
@@ -38,68 +41,229 @@ export default defineConfig({
     // 项目描述
     description: "VitePress Test",
 
+    // 外观切换，如果设置为true，将在导航栏中显示一个切换按钮，允许用户在浅色和深色主题之间进行选择。默认不写就有。
+    appearance: true,
+
+    // 当设置为 true 时，VitePress 不会因为死链接而导致构建失败。默认为 false。
+    ignoreDeadLinks: false,
+    // 显示页面最后更新时间
+    lastUpdated: true,
+    // 设为true删除路径中的.html后缀,设为false则保留.html后缀,默认为false
+    cleanUrls: false,
     //主题配置
     themeConfig: {
-        // 首页项目标题前面的图标
+        // 首页左上角标题前面和左侧导航栏的图标
         logo: '/StreamlineEmojisChicken.svg',
+
+        // 首页左上角和左侧导航栏的标题,没写的话默认是index.md的hero.name
+        // 隐藏站点标题文本 => 设置为 siteTitle: false隐藏
+        siteTitle: 'Hello VitePress',
+
+        // 大纲的标题 => 右侧小标题的标题大纲
+        outlineTitle: '快速导航',
+
+        // 在大纲中显示的标题级别
+        outline: 'deep',
+
+        // 最近更新时间显示的文本
+        lastUpdatedText: '最近更新',
+
+        // 文档底部文本
+        docFooter: {
+            prev: '上一页',
+            next: '下一页'
+        },
+        carbonAds: {
+            code: 'your-carbon-code',
+            placement: 'your-carbon-placement'
+        },
+
+        // 有 sidebar 时不显示
+        footer: {
+            message: 'Released under the <a href="https://github.com/vuejs/vitepress/blob/main/LICENSE">MIT License</a>.',
+            copyright: 'Copyright © 2019-present <a href="https://github.com/yyx990803">Evan You</a>'
+        },
 
         // 编辑链接的信息。如果未定义，编辑链接功能将被禁用。它位于页面底部。
         editLink: {
             text: '为此页提供修改建议',
-            pattern: 'https://github.com/honghuangdc/soybean-admin-docs/tree/main/:path'
+            pattern: 'https://gitee.com/wakemoto/vite-press-system-documents-main-navigation/commit/5d9b9623f0122176e3bd8170837b38326efec89f'
         },
 
         // 要在导航栏末尾显示的社交链接。非常适合放置指向GitHub，Twitter，Facebook等社交服务的链接。
         socialLinks: [
-            { icon: 'github', link: 'https://gitee.com/wakemoto/vite-press-system-documents-main-navigation/commit/cbb2edd36297be567dfe9e7eac81602481564627' },
-            {icon: {svg: qqSvg}, link: 'https://qm.qq.com/cgi-bin/qm/qr?k=XTjkqjV1KynFoEAdrqxMd6i71-TtCVGV&jump_from=webapi'}
+            {
+                icon: 'github',
+                link: 'https://gitee.com/wakemoto/vite-press-system-documents-main-navigation/commit/cbb2edd36297be567dfe9e7eac81602481564627'
+            },
+            {
+                icon: {svg: qqSvg},
+                link: 'https://qm.qq.com/cgi-bin/qm/qr?k=XTjkqjV1KynFoEAdrqxMd6i71-TtCVGV&jump_from=webapi'
+            }
         ],
         // https://www.algolia.com/developers/ 一个全局搜索引擎
         // 使用前需要申请appId,appKey,indexName
-        algolia: {
-            appId: 'XDL85IJNCD',
-            apiKey: 'd24df6b33bfb43718acaa6fcf8276f71',
-            indexName: 'systemDocument',
-            // 这是搜索输入框的占位符文本，用于在输入框为空时显示提示性文字。该属性是可选的，可以不提供，如果不提供，则输入框将没有占位符文本。
-            placeholder: '搜索文档',
-            // 可以用不同的语言本地化搜索输入框的标签、按钮文本等。该属性是可选的，可以不提供，如果不提供，则使用默认的文本。
-            translations: {
-                button: {
-                    buttonText: '搜索',
+        search: {
+            provider: 'algolia',
+            options: {
+                appId: 'XDL85IJNCD',
+                apiKey: 'd24df6b33bfb43718acaa6fcf8276f71',
+                indexName: 'systemDocument',
+                // 这是搜索输入框的占位符文本，用于在输入框为空时显示提示性文字。该属性是可选的，可以不提供，如果不提供，则输入框将没有占位符文本。
+                placeholder: '搜索文档',
+                // 可以用不同的语言本地化搜索输入框的标签、按钮文本等。该属性是可选的，可以不提供，如果不提供，则使用默认的文本。
+                translations: {
+                    button: {
+                        buttonText: '搜索',
+                    },
                 },
+                locales: {
+                    //这里是个大坑，zh是不生效的，改为root即可
+                    root: {
+                        placeholder: '搜索文档',
+                        translations: {
+                            button: {
+                                buttonText: '搜索文档',
+                                // 残障人士使用屏幕阅读器对网络内容的访问
+                                buttonAriaLabel: '搜索文档'
+                            },
+                            modal: {
+                                // 搜索框
+                                searchBox: {
+                                    // 重置按钮标题
+                                    resetButtonTitle: '清除查询条件',
+                                    resetButtonAriaLabel: '清除查询条件',
+                                    // 取消按钮文本
+                                    cancelButtonText: '取消',
+                                    cancelButtonAriaLabel: '取消'
+                                },
+                                // 开始屏幕
+                                startScreen: {
+                                    // 最近的搜索标题
+                                    recentSearchesTitle: '搜索历史',
+                                    // 没有“最近搜索”文本
+                                    noRecentSearchesText: '没有搜索历史',
+                                    // 保存“最近的搜索”按钮标题
+                                    saveRecentSearchButtonTitle: '保存至搜索历史',
+                                    // 删除“最近搜索”按钮标题
+                                    removeRecentSearchButtonTitle: '从搜索历史中移除',
+                                    // 收藏夹 搜索标题
+                                    favoriteSearchesTitle: '收藏',
+                                    // 删除收藏夹搜索按钮标题
+                                    removeFavoriteSearchButtonTitle: '从收藏中移除'
+                                },
+                                // 错误屏幕
+                                errorScreen: {
+                                    // 标题文本
+                                    titleText: '无法获取结果',
+                                    // 帮助文本
+                                    helpText: '你可能需要检查你的网络连接'
+                                },
+                                // 搜索框的底部
+                                footer: {
+                                    selectText: '选择',
+                                    navigateText: '切换',
+                                    closeText: '关闭',
+                                    searchByText: '搜索提供者'
+                                },
+                                // 无结果屏幕
+                                noResultsScreen: {
+                                    // 无结果文本
+                                    noResultsText: '无法找到相关结果',
+                                    // 建议的查询文本
+                                    suggestedQueryText: '你可以尝试查询',
+                                    // 报告缺少结果文本
+                                    reportMissingResultsText: '你认为该查询应该有结果？',
+                                    // 报告缺少结果链接文本
+                                    reportMissingResultsLinkText: '点击反馈'
+                                }
+                            }
+                        }
+                    }
+                }
             },
         },
         // https://vitepress.dev/reference/default-theme-config
         // 导航项。text是导航栏显示的文本,link是Markdown文件的文件名不需要后缀
+        // 如果文件名叫index.md可以省略不写，路径只写到目录名就可以了
+        // activeMatch: '/config/' => 当前页面的导航栏高亮
         nav: [
             {text: '首页', link: '/'},
-            {text: '例子', link: '/markdown-examples'}
-        ],
-
-        // 侧边栏项目。
-        sidebar: [
+            {text: '例子', link: '/examples/markdown-examples', activeMatch: '/examples/'},
             {
-                // text是侧边栏显示的文本
-                text: 'Examples',
-                // 项的子项。
+                text: '博客文档',
+                // 可以配置成下拉菜单
                 items: [
-                    {text: 'Markdown Examples', link: '/markdown-examples'},
-                    {text: 'Runtime API Examples', link: '/api-examples'}
-                ]
-            },
-            {
-                text: 'Demo',
-                items: [
-                    {text: 'Demo1', link: '/demo/demo1'},
-                    {text: 'Demo2', link: '/demo/demo2'}
+                    {text: 'JavaScript 核心系列', link: '/articles/javaScript-core/构造函数、原型、原型链'},
+                    {text: 'Vue 三方组件库', link: '/articles/libs/VForm3低代码初体验'},
+                    {text: '其他', link: '/articles/other/nvm管理node'},
                 ]
             }
 
         ],
+
+        sidebar: {
+            '/examples/': [
+                {
+                    // text是侧边栏显示的文本
+                    text: 'Examples',
+                    // 项的子项。
+                    items: [
+                        {text: 'Markdown Examples', link: '/examples/markdown-examples'},
+                        {text: 'Runtime API Examples', link: '/examples/api-examples'}
+                    ]
+                },
+                {
+                    text: 'Demo',
+                    // 侧边栏是否展开，false是展开,true是折叠,默认值是 false。
+                    //collapsed: false,
+                    items: [
+                        {text: 'Demo1', link: '/demo/demo1'},
+                        {text: 'Demo2', link: '/demo/demo2'}
+                    ]
+                }
+            ],
+
+            '/demo/': [
+                {
+                    text: 'Demo',
+                    items: [
+                        {text: 'Demo1', link: '/demo/demo1'},
+                        {text: 'Demo2', link: '/demo/demo2'}
+                    ]
+                }
+            ]
+        },
+        // 侧边栏项目。
+        // sidebar: [
+        //     {
+        //         // text是侧边栏显示的文本
+        //         text: 'Examples',
+        //         // 项的子项。
+        //         items: [
+        //             {text: 'Markdown Examples', link: '/markdown-examples'},
+        //             {text: 'Runtime API Examples', link: '/api-examples'}
+        //         ]
+        //     },
+        //     {
+        //         text: 'Demo',
+        //         items: [
+        //             {text: 'Demo1', link: '/demo/demo1'},
+        //             {text: 'Demo2', link: '/demo/demo2'}
+        //         ]
+        //     }
+        //
+        // ],
+        // 移动设备才能看到，PC端看不到
+        returnToTopLabel: '返回顶部',
+
     },
+
 
     // 用于配置markdown-it的选项。注意，你不能使用此选项覆盖默认的markdown-it选项，只能用于扩展它。
     markdown: {
+        // 显示行号
+        lineNumbers: true,
         // 锚点配置
         anchor: {}
     }
--- docs/index.md
+++ docs/index.md
@@ -1,57 +1,142 @@
 ---
+
 # https://vitepress.dev/reference/default-theme-home-page
+
 layout: home
 
 # 网站图标旁边的显示文字
+
 title: liuB 个人博客
 titleTemplate: 开发文档
-hero:
-  # 项目名称
-  name: "VitePress"
-  # 项目的简单描述
-  text: "VitePress开发文档"
-  # 标语：基于什么技术栈
-  tagline: "基于Vue3、Vite3、TS、NaiveUI和UnoCSS"
 
-  # 项目首页的logo
-  image: 
-    # 相对于public文件夹的路径
-    src: /StreamlineEmojis2.svg
-    # 图片不显示时的文字
-    alt: Yellow Chicken
-  actions:
-    - theme: brand
-      text: 开始
-      link: /markdown-examples
-    - theme: alt
-      text: 介绍
-      link: /demo/demo1
-    - theme: alt
-      text: 在Gitee上查看
-      link: https://gitee.com/wakemoto/projects
-    - theme: brand
-      text: v1.0.0
+hero:
+# 项目名称
+    name: "VitePress"
+# 项目的简单描述
+    text: "VitePress开发文档"
+# 标语：基于什么技术栈
+    tagline: "基于Vue3、Vite3、TS、NaiveUI和UnoCSS"
+# 项目首页的logo
+    image:
+# 相对于public文件夹的路径 ,放在public中无需写完整路径
+          src: /StreamlineEmojis2.svg
+# 图片不显示时的文字
+          alt: Yellow Chicken
+    actions:
+            - theme: brand
+              text: 开始
+              link: /examples/markdown-examples
+            - theme: alt
+              text: 介绍
+              link: /demo/demo1
+            - theme: alt
+              text: 在Gitee上查看
+              link: https://gitee.com/wakemoto/projects
+            - theme: brand
+              text: v1.0.0
 
 features:
-  # 直接复制粘贴即可 https://github.com/markdown-it/markdown-it-emoji/blob/master/lib/data/full.json
-  - icon: 🐳
-    title: 最新流行技术栈
-    details: 基于Vue3、Vite3、TS、NaiveUI和UnoCSS等最新技术栈开发
-  - icon: 🦈
-    title: 最新流行技术栈
-    details: 基于Vue3、Vite3、TS、NaiveUI和UnoCSS等最新技术栈开发
-  - icon: 🦭
-    title: 最新流行技术栈
-    details: 基于Vue3、Vite3、TS、NaiveUI和UnoCSS等最新技术栈开发
-  - icon: 🐧
-    title: 最新流行技术栈
-    details: 基于Vue3、Vite3、TS、NaiveUI和UnoCSS等最新技术栈开发
-  - icon: 🐬
-    title: 最新流行技术栈
-    details: 基于Vue3、Vite3、TS、NaiveUI和UnoCSS等最新技术栈开发
-  - icon:
-      src: /StreamlineEmojisBird2.svg
-    title: 最新流行技术栈
-    details: 基于Vue3、Vite3、TS、NaiveUI和UnoCSS等最新技术栈开发
+
+# 直接复制粘贴即可 https://github.com/markdown-it/markdown-it-emoji/blob/master/lib/data/full.json
+
+- icon: 🐳
+  title: 最新流行技术栈
+  details: 基于Vue3、Vite3、TS、NaiveUI和UnoCSS等最新技术栈开发
+  link: /examples/markdown-examples
+  linkText: 开始刷题
+- icon: 🦈
+  title: 最新流行技术栈
+  details: 基于Vue3、Vite3、TS、NaiveUI和UnoCSS等最新技术栈开发
+- icon: 🦭
+  title: 最新流行技术栈
+  details: 基于Vue3、Vite3、TS、NaiveUI和UnoCSS等最新技术栈开发
+- icon: 🐧
+  title: 最新流行技术栈
+  details: 基于Vue3、Vite3、TS、NaiveUI和UnoCSS等最新技术栈开发
+- icon: 🐬
+  title: 最新流行技术栈
+  details: 基于Vue3、Vite3、TS、NaiveUI和UnoCSS等最新技术栈开发
+- icon:
+# 这里一不小心就会报：Uncaught (in promise) TypeError: Cannot read properties of null (reading 'alt')
+       src: /StreamlineEmojisBird2.svg
+  title: 最新流行技术栈
+  details: 基于Vue3、Vite3、TS、NaiveUI和UnoCSS等最新技术栈开发
+
+# TeamMembers:  团队成员
+# avatar: 成员的头像,这里使用的是github的头像
+# name:  成员的名称
+# title: 成员的职位
+# org: 成员所在的组织
+# orgLink: 成员所在的组织的链接
+# desc: 成员的描述
+# links: 成员的社交链接
+# icon: 社交链接的图标
+# link: 社交链接的链接
+# sponsor: 成员的赞助商
 ---
 
+<script setup>
+import {
+  VPTeamPage,
+  VPTeamPageTitle,
+  VPTeamMembers,
+  VPTeamPageSection
+} from 'vitepress/theme';
+import { icons } from './socialIcons';
+
+const coreMembers = [  
+  {
+    avatar: 'https://www.github.com/HearLing.png',
+    name: 'HearLing',
+    title: '热爱学习，不秃头',
+    desc: 'FE Developer',
+    links: [
+      { icon: 'github', link: 'https://github.com/HearLing'},
+      {icon: { svg: icons.bilibili } ,link: "https://space.bilibili.com/201738571"},
+    ]
+  },
+  {
+    avatar: 'https://github.com/WakeMeTo.png',
+    name: 'Bin Liu',
+    title: '逆水行舟，不进则退',
+    desc: 'FE Developer<br/>Creator @ <a href="https://github.com/chodocs/chodocs" target="_blank">ChoDocs</a>',
+    org: 'Vue.js',
+    orgLink:'https://gitee.com/wakemoto/vite-press-system-documents-main-navigation',
+    links: [
+      { icon: 'github', link: 'https://github.com/yyx990803' },
+      { icon: 'twitter', link: 'https://twitter.com/youyuxi' }
+    ],
+    sponsor:'https://gitee.com/wakemoto/'
+  },
+];
+const partners = [
+  {
+    avatar: 'https://www.github.com/yyx990803.png',
+    name: 'Evan You',
+    title: 'Creator',
+    org: 'Vue.js',
+    orgLink:'https://gitee.com/wakemoto/vite-press-system-documents-main-navigation',
+    desc: 'Evan You is the creator of Vue.js.',
+    links: [
+      { icon: 'github', link: 'https://github.com/yyx990803' },
+      { icon: 'twitter', link: 'https://twitter.com/youyuxi' }
+    ],
+    sponsor:'https://gitee.com/wakemoto/'
+  },
+];
+</script>
+
+<VPTeamPage>
+  <VPTeamPageTitle>
+    <template #title><Badge type="tip" text="核心成员" /></template>
+    <template #lead>...</template>
+  </VPTeamPageTitle>
+  <VPTeamMembers size="small" :members="coreMembers" />
+  <VPTeamPageSection>
+    <template #title><Badge type="tip" text="友情链接" /></template>
+    <template #lead>...</template>
+    <template #members>
+      <VPTeamMembers size="small" :members="partners" />
+    </template>
+  </VPTeamPageSection>
+</VPTeamPage>
